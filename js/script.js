f6ecfef6ba96394416514c6f99cc67b99438d1bc
Moses Lee <leecmoses@gmail.com>
2018-12-17 18:54:22 -0500

    Redo portfolio website

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-var scrollLink = $('.scroll');
-    scrollLink.click(function(e){
-        e.preventDefault();
-        let linkRef = $(this).attr('href');
-        let position = $(linkRef).offset().top;
-        $('html, body').animate({
-            scrollTop : position
-            }, 1000)
-})
+/////////////////////
+// Navigation Menu //
+/////////////////////
+let menu = document.querySelector('#menu');
+let page = document.querySelectorAll('header, main, footer');
+let nav = document.querySelector('.nav');
+
+menu.addEventListener('click', function(e) {
+    nav.classList.toggle('open');
+    e.stopPropagation();
+});
+
+Array.prototype.forEach.call(page, function(el) {
+    el.addEventListener('click', function() {
+        nav.classList.remove('open');
+    })
+});
